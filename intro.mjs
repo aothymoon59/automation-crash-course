755e2713df8632da1a460344cfc168214ab3879c
aothymoon59 <aothymoon59@gmail.com>
2024-04-07 23:52:02 +0600

    make another screenshort automation

--- intro.mjs
+++ intro.mjs
@@ -4,14 +4,21 @@ const browser = await puppeteer.launch({
   headless: false,
   defaultViewport: { width: 1920, height: 1080 },
   slowMo: 250,
-  userDataDir: "temporary",
 });
 
 const page = await browser.newPage();
-await page.goto("https://example.com", {
-  waitUntil: "networkidle2",
-  timeout: 60000,
-});
+await page.goto("https://example.com");
+await page.screenshot({ path: "screenshot.png" });
 
-await page.screenshot({ path: "example.com.png" });
+await page.goto("https://devconfbd.com/");
+await page.screenshot({ path: "devconfbd.png" });
+
+// await setTimeout(1000);
+const guestElement = await page.waitForSelector("img[alt='guest']");
+await guestElement.scrollIntoView();
+// await setTimeout(1000);
+await page.click("img[alt='guest']");
+// await setTimeout(1000);
+await page.screenshot({ path: "guest.png" });
+
 await browser.close();
